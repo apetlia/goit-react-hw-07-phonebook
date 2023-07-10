e2b6bd5d11711148fa02f04ce5bf7eeb2ad3221a
Artem Petlia <pak@servers.com>
2023-07-10 13:35:39 +0300

    add src template

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = {
+  colors: {
+    black: '#000000',
+    gray: '#808080',
+    silver: '#C0C0C0',
+    lightGray: '#D3D3D3',
+    whiteSmoke: '#F5F5F5',
+    white: '#FFFFFF',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
